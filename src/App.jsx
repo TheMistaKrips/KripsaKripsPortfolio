d472bed2357c7ddee0bfa0b97c70efc6cc586580
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2024-01-03 22:13:59 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/PpTnkN3YI9AunEKd/scene.splinecode" />
+  );
+}
